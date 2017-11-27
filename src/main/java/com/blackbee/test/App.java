e7abd49e6cd9dc0680ea2a4beeafdc996650749a
Roman <postnikovrom@gmail.com>
2017-11-27 17:23:17 +0200

    Looking for links with main()

--- src/main/java/com/blackbee/test/App.java
+++ src/main/java/com/blackbee/test/App.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -13,58 +14,37 @@ import java.io.IOException;
  */
 public class App {
     public static void main(String[] args) throws IOException {
-        /*String html = "<html><head><title>First parse</title></head>"
-                + "<body><p>Parsed HTML into a doc.</p></body></html>";
-        Document doc = Jsoup.parse(html);
-        System.out.println(doc.toString());
+        String keyword = "ducks";
+        Document doc = Jsoup.connect("https://www.aboutyou.de/suche?term=" + keyword).get();
 
-        String html1 = "<div><p>Lorem ipsum.</p>";
-        Document doc1 = Jsoup.parseBodyFragment(html);
-        Element body = doc.body();
-        System.out.println(body.toString());
+        Elements search = doc.getElementsByClass("row list-wrapper product-image-list");
+        Element searchDiv = search.get(0);
 
-        Document doc2 = Jsoup.connect("https://www.aboutyou.de/").get();
-        String title = doc2.title();
-        System.out.println(title);
+        //Elements links = searchDiv.getElementsByTag("a");
+        Elements links = searchDiv.select("a[href]");
+        System.out.println(links);
 
 
 
-        File input = new File("input.html");
-        Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
 
-        Elements links = doc.select("a[href]"); // a with href
-        Elements pngs = doc.select("img[src$=.png]");
-        // img with src ending .png
-
-        Element masthead = doc.select("div.masthead").first();
-        // div with class=masthead
-
-        Elements resultLinks = doc.select("h3.r > a"); // direct a after h3*/
-
-        Document doc = Jsoup.connect("https://www.aboutyou.de/suche?term=ducks&category=20201").get();
+       /* Document doc = Jsoup.connect("https://www.aboutyou.de/p/save-the-duck/steppjacke-mit-kunstdaunen-3727286").get();
+        Elements info = doc.getElementsByClass("rc-tooltip-inner");
 
 
-        Element content = doc.getElementById("content");
-        Elements links = doc.getElementsByTag("a");
-        for (Element link : links) {
-            String linkHref = link.attr("href");
-            String linkText = link.text();
+        try(FileWriter writer = new FileWriter("output.xml", false))
+        {
+            // запись всей строки
+            //String text = "tu carne";
+            writer.write(info.toString());
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
         }
-        Elements linkies = doc.select("a[href]"); // a with href
-        Elements pngs = doc.select("img[src$=.png]");
-        // img with src ending .png
+        catch(IOException ex){
 
-        Element masthead = doc.select("div.masthead").first();
-        // div with class=masthead
-
-        Elements resultLinks = doc.select("h3.r > a"); // direct a after h3
-
-       // System.out.println(linkies.toString() + "\n");
-        System.out.println(pngs.toString() + "\n");
-        //System.out.println(masthead.toString() + "\n\n");
-        System.out.println(resultLinks.toString() + "\n");
-
-        File output = new File("output.xml");
-
+            System.out.println(ex.getMessage());
+        }*/
     }
 }
---
+++ src/main/java/com/blackbee/test/service/Output.java
@@ -0,0 +1,17 @@
+package com.blackbee.test.service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Output {
+   /* public static void writeToXml(String text, File file){
+        OutputStream outputStream = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+
+            }
+        }
+    }*/
+}
